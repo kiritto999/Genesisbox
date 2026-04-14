5dc7fa46c66bcc563114b793c8f9d510193e47da
ko9 <kiritto999kun@gmail.comD>
2026-04-14 01:53:48 -0500

    Primmer commit

---
+++ src/genesisbox_galaxy/main/main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package genesisbox_galaxy.main;
+
+/**
+ *
+ * @author blope
+ */
+public class main {
+    
+}
